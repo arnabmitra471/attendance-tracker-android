135d9bc9371ff850b89e5d9e6e75977e87d2d781
Arnab Mitra <arnabmitra471@gmail.com>
2024-04-27 20:06:09 +0530

    Woring on the addattendance alert dialog

--- app/src/main/java/com/example/attendancetracker/AttendanceActivity.java
+++ app/src/main/java/com/example/attendancetracker/AttendanceActivity.java
@@ -1,10 +1,15 @@
 package com.example.attendancetracker;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
@@ -16,6 +21,7 @@ public class AttendanceActivity extends AppCompatActivity {
 
     TextView dayTxt;
     ListView attendance_list;
+    Button attAdd;
 
     ArrayList<AttendanceData> attendanceData = new ArrayList<>();
     @Override
@@ -31,6 +37,7 @@ public class AttendanceActivity extends AppCompatActivity {
 
         dayTxt = findViewById(R.id.dayTxt);
         attendance_list = findViewById(R.id.attendance_list);
+        attAdd = findViewById(R.id.attAdd);
         Bundle bundle = getIntent().getExtras();
 
         dayTxt.setText(bundle.getString("Day"));
@@ -50,5 +57,23 @@ public class AttendanceActivity extends AppCompatActivity {
 
         AttendanceAdapter attAdapter = new AttendanceAdapter(AttendanceActivity.this,attendanceData);
         attendance_list.setAdapter(attAdapter);
+
+        attAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+    }
+    private void showAddAttendanceDialog(){
+        AlertDialog.Builder builder = new AlertDialog.Builder(AttendanceActivity.this);
+        View dialogView = getLayoutInflater().inflate(R.layout.add_attendance_dialog,null);
+        builder.setView(dialogView);
+
+        TextView dateTxt = dialogView.findViewById(R.id.dateTxt);
+        EditText s_nameTxt = dialogView.findViewById(R.id.att_nameTxt);
+        EditText s_rollNoTxt = dialogView.findViewById(R.id.att_rollNoTxt);
+        Spinner attStatus = dialogView.findViewById(R.id.statusOpt);
+
     }
 }
